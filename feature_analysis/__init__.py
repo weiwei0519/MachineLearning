a4d2d47c6e8e207b7b7fe6d3df62bd0945b70edb
WeiWei_519 <WeiWei_519@outlook.com>
2022-09-08 23:28:14 +0800

    pandas data analysis 20220908

---
+++ feature_analysis/__init__.py
@@ -0,0 +1,11 @@
+# coding=UTF-8
+# 
+# 
+
+'''
+@File: __init__.py.py
+@Author: Wei Wei
+@Time: 2022/9/8 21:42
+@Email: weiwei_519@outlook.com
+@Software: PyCharm
+'''
---
+++ feature_analysis/features.py
@@ -0,0 +1,59 @@
+# coding=UTF-8
+# 特征选择的分析方法
+# 
+
+'''
+@File: features.py
+@Author: Wei Wei
+@Time: 2022/9/8 21:42
+@Email: weiwei_519@outlook.com
+@Software: PyCharm
+'''
+
+import pandas as pd
+import pandas_profiling
+from utils.pathutil import PathUtil
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn import preprocessing
+
+project_path = PathUtil()
+# data = 'https://raw.githubusercontent.com/pycaret/pycaret/master/datasets/automobile.csv'
+data_path = project_path.rootPath + '/datasets/data/income_trainsets.csv'
+data = pd.read_csv(data_path)
+data.info()
+data = data.sample(1000)
+# 对非数值型变量进行预处理
+f_names = ['workclass', 'education', 'marital', 'occupation', 'relationship', 'race', 'sex', 'native-country', 'income']
+for x in f_names:
+    label = preprocessing.LabelEncoder()
+    data[x] = label.fit_transform(data[x])
+
+# pfr = pandas_profiling.ProfileReport(data)
+# pfr.to_file("./profile_report.html")
+
+# (data.corr().loc[:, :13].plot(kind='barh', figsize=(4, 10)))
+# pd.plotting.scatter_matrix(data,
+#                            figsize=(14, 14),
+#                            c='k',
+#                            marker='o',
+#                            diagonal='hist',
+#                            alpha=0.8,
+#                            range_padding=0.1)
+
+corr_data = data.corr()
+f, ax = plt.subplots(figsize=(14, 9))
+sns.heatmap(corr_data, vmax=0.8, square=True)
+cols = corr_data.nlargest(14, 'income')['income'].index
+cm = np.corrcoef(data[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm,
+                 cbar=True,
+                 annot=True,
+                 square=True,
+                 fmt='.2f',
+                 annot_kws={'size': 10},
+                 yticklabels=cols.values,
+                 xticklabels=cols.values)
+plt.show()
